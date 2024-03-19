c61ba778a28e2cea57a12b22915cc593f77d2a94
Alliya De Guzman <alli.socialacc@gmail.com>
2024-03-19 11:20:40 +0800

    Alliya

---
+++ GROUP1/src/components/pages/Member2/member2.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+import "./member2styles.css"
+
+function member2() {
+
+  return (
+    <div className="content">
+      
+
+      <h2>De Guzman, Alliya Lley B. SN: 2019135257</h2>
+      
+
+    </div>  
+  )
+}
+
+export default member2
