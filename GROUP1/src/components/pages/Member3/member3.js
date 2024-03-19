1d8262387cf56aa4965092eeccaab48ad16722e4
Le1ghZ <kamanalo@mymail.edu.ph>
2024-03-19 11:22:25 +0800

    Kurt

---
+++ GROUP1/src/components/pages/Member3/member3.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+import "./member3styles.css"
+
+function member3() {
+
+  return (
+    <div className="content">
+      
+
+      <h2>Manalo, Kurt Aidan A. SN: 2019138072</h2>
+      
+
+    </div>  
+  )
+}
+
+export default member3
