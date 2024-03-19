bb870c6c94c8a06eca5ebdc72a041e6eba1e0817
Harley <harleyyramone@gmail.com>
2024-03-19 11:21:35 +0800

    member4-Harley

---
+++ GROUP1/src/components/pages/Member4/member4.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+import "./member4styles.css"
+
+function member4() {
+
+  return (
+    <div className="content">
+      
+
+      <h2>Tesorero, Harley Ramone R. SN: 2021105456</h2>
+      
+
+    </div>  
+  )
+}
+
+export default member4
